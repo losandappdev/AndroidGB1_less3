db779bfc53ca6424f2c84be53984ddc23b4993f3
Anton Lo <antshot19@gmail.com>
2019-08-02 14:29:08 +0300

    Task3.

--- src/main/java/com/n/lesson3/MainActivity.java
+++ src/main/java/com/n/lesson3/MainActivity.java
@@ -19,47 +19,47 @@ public class MainActivity extends AppCompatActivity {
     private EditText editText1;
     private EditText editText2;
 
-    Button simpleButton = (Button) findViewById(R.id.simpleButton);
+//    Button simpleButton = findViewById(R.id.simpleButton);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
 
-        if (savedInstanceState != null){
-            String str1 = savedInstanceState.getString(EXTRA_KEY);
-            String str2 = savedInstanceState.getString(EXTRA_KEY1);
-        }
+//        if (savedInstanceState != null){
+//            String str1 = savedInstanceState.getString(EXTRA_KEY);
+//            String str2 = savedInstanceState.getString(EXTRA_KEY1);
+//        }
+//
+//        editText1 = findViewById(R.id.editText1);
+//        editText2 = findViewById(R.id.editText2);
 
-        editText1 = findViewById(R.id.editText1);
-        editText2 = findViewById(R.id.editText2);
-
-        simpleButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
+//        simpleButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
 
     }
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        String str1 = editText1.getText().toString();
-        String str2 = editText2.getText().toString();
+//        String str1 = editText1.getText().toString();
+//        String str2 = editText2.getText().toString();
+//
+//        outState.putString(EXTRA_KEY, str1);
+//        outState.putString(EXTRA_KEY1, str2);
 
-        outState.putString(EXTRA_KEY, str1);
-        outState.putString(EXTRA_KEY1, str2);
-
-        Log.d(TAG, "onSaveInstanceState: " + str1 + str2);
+//        Log.d(TAG, "onSaveInstanceState: " + str1 + str2);
 
     }
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        String str1 = savedInstanceState.getString(EXTRA_KEY);
-        String str2 = savedInstanceState.getString(EXTRA_KEY1);
-        editText1.setText(str1);
-        editText2.setText(str2);
+//        String str1 = savedInstanceState.getString(EXTRA_KEY);
+//        String str2 = savedInstanceState.getString(EXTRA_KEY1);
+//        editText1.setText(str1);
+//        editText2.setText(str2);
 
     }
 
