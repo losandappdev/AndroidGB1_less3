a325196c4abac06b7862eab5f6a0bed9839c1426
Anton Lo <antshot19@gmail.com>
2019-08-06 17:56:16 +0300

    Task3.

--- src/main/java/com/n/lesson3/Main2Activity.java
+++ src/main/java/com/n/lesson3/Main2Activity.java
@@ -3,12 +3,34 @@ package com.n.lesson3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    private static final String TAG = "Main2Activity";
+
+    public static final String EXTRA_KEY1 ="EXTRA_KEY1";
+    public static final String EXTRA_KEY2 ="EXTRA_KEY2";
+
+    private TextView textView1;
+    private  TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+       String str =  getIntent().getStringExtra(EXTRA_KEY1);
+       int x = getIntent().getIntExtra(EXTRA_KEY2, -1);
+
+        Log.d(TAG, "onCreate: "+ str);
+        Log.d(TAG, "onCreate: " + x);
+
+        textView1 = findViewById(R.id.textView1);
+        textView2 = findViewById(R.id.textView2);
+//
+        textView1.setText(str);
+        textView2.setText(String.valueOf(x));
     }
 }
--- src/main/java/com/n/lesson3/MainActivity.java
+++ src/main/java/com/n/lesson3/MainActivity.java
@@ -3,6 +3,7 @@ package com.n.lesson3;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,55 +14,65 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    private static final String EXTRA_KEY = "extraKey";
-    public static final String EXTRA_KEY1 = "extraKey1";
-
     private EditText editText1;
     private EditText editText2;
+    private Button button;
 
-//    Button simpleButton = findViewById(R.id.simpleButton);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
 //        if (savedInstanceState != null){
 //            String str1 = savedInstanceState.getString(EXTRA_KEY);
 //            String str2 = savedInstanceState.getString(EXTRA_KEY1);
 //        }
 //
-//        editText1 = findViewById(R.id.editText1);
-//        editText2 = findViewById(R.id.editText2);
+        editText1 = findViewById(R.id.editText1);
+        editText2 = findViewById(R.id.editText2);
+        button = findViewById(R.id.simpleButton);
 
-//        simpleButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//            }
-//        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String str1 = editText1.getText().toString();
+                String str2 = editText2.getText().toString();
+                Log.d(TAG, "onClick: "+ str1 + str2);
+
+
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra(Main2Activity.EXTRA_KEY1, editText1.getText().toString());
+
+                int x = Integer.parseInt(editText2.getText().toString());
+                intent.putExtra(Main2Activity.EXTRA_KEY1, x);
+
+                startActivity(intent);
+            }
+        });
 
     }
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
+//    @Override
+//    protected void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
 //        String str1 = editText1.getText().toString();
 //        String str2 = editText2.getText().toString();
 //
 //        outState.putString(EXTRA_KEY, str1);
 //        outState.putString(EXTRA_KEY1, str2);
-
+//
 //        Log.d(TAG, "onSaveInstanceState: " + str1 + str2);
-
-    }
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
+//
+//    }
+//    @Override
+//    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+//        super.onRestoreInstanceState(savedInstanceState);
 //        String str1 = savedInstanceState.getString(EXTRA_KEY);
 //        String str2 = savedInstanceState.getString(EXTRA_KEY1);
 //        editText1.setText(str1);
 //        editText2.setText(str2);
-
-    }
+//
+//    }
 
 
 }
